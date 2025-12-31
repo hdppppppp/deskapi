8de20eadd1db6c2783672042049dcbf82dd4f0a3
hdppppppp <177169374@qq.com>
2026-01-01 01:26:46 +0800

    继续更新

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["desk.wdihgt.com"];
+pub const RS_PUB_KEY: &str = "kCUqx3faXIQFWwHEyg7ieajohP3BOR4hzKiu0wRRizE=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
